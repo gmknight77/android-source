ec41369226e3cd0eb6ff0fa9cf6193fa7e4594bc
ERH <gmknight77@gmail.com>
2014-05-05 08:19:23 -0400

    Corrections to WorstPractices

--- milestone_00_welcome_to_android_development/03_basic_java/best_practices/src/com/bloc/bestpractices/WorstPractices.java
+++ milestone_00_welcome_to_android_development/03_basic_java/best_practices/src/com/bloc/bestpractices/WorstPractices.java
@@ -9,18 +9,19 @@ public class WorstPractices extends Object {
 // EDIT BELOW
 
 	public static void main(String [] args) {
-	int MAGIC_NUMBER = WorstPractices.animals(false);
-	MAGIC_NUMBER *= 5;
-	if (MAGIC_NUMBER > 18) {
-	while(MAGIC_NUMBER > 0)
-	{
-	MAGIC_NUMBER--;
-	}
-	}
+		int MAGIC_NUMBER = WorstPractices.animals(false);
+		MAGIC_NUMBER *= 5;
+			if (MAGIC_NUMBER > 18) {
+									while(MAGIC_NUMBER > 0)
+								{
+				MAGIC_NUMBER--;
+									}
+			}
 	}
 
 // animals
-// this method takes in a single parameter, yeswellheresthething. Using a very elaborate and complex algorithm, it calculate a magic number
+// this method takes in a single parameter, yeswellheresthething. 
+//Using a very elaborate and complex algorithm, it calculate a magic number
 // yeswellheresthething: a seed which helps generate the magic number
 // returns: a magical number
 private static int animals(boolean yeswellheresthething)
@@ -30,8 +31,10 @@ private static int animals(boolean yeswellheresthething)
 		*/
 	int anint = yeswellheresthething ? 34 : 21;
 	float sparklesfairy = .5f;
-	for (int brown = 0; brown < aInt; brown++) { sparklesfairy *= aInt;
-	} return (int) sparklesfairy * aInt;
+	for (int brown = 0; brown < aInt; brown++) 
+		{ sparklesfairy *= aInt;
+			} 
+	return (int) sparklesfairy * aInt;
 }
 
 // STOP EDITING
