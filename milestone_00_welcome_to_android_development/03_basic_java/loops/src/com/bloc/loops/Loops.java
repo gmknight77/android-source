882d481d1bf9e282f360d4ebe3bc9091340c71d5
ERH <gmknight77@gmail.com>
2014-05-09 11:34:07 -0400

    Update 4 of Loops File ERH

--- milestone_00_welcome_to_android_development/03_basic_java/loops/src/com/bloc/loops/Loops.java
+++ milestone_00_welcome_to_android_development/03_basic_java/loops/src/com/bloc/loops/Loops.java
@@ -1,9 +1,5 @@
 package com.bloc.loops;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
 import com.bloc.test.Test;
 
 public class Loops extends Object {
@@ -13,6 +9,7 @@ public class Loops extends Object {
 		boolean temp = false;
 
 		// Starts here
+		/*
 		temp = someBools[7];
 		someBools[7] = someBools[0];
 		someBools[0] = temp;
@@ -27,31 +24,58 @@ public class Loops extends Object {
 
 		temp = someBools[4];
 		someBools[4] = someBools[3];
-		someBools[3] = temp;
+		someBools[3] = temp;*/
 		// Ends here
-		
-		      while (somebools && temp == true)
-      		{
-      				
-
-      		}
+		/*
+		int i = 0;
+		int farIndex = 0;
+				while (i < (someBools.length / 2)) // i < 3
+        {
+        	// Loop through and reverse the array
+        	farIndex = someBools.length - 1 - i;
+        	temp = someBools[farIndex];
+        	someBools[i] = someBools[farIndex];
+        	someBools[farIndex] = temp;
+        */	
+		int n =7;
+		int d =0;
+		while (n <= 7 && d <= 3) {
+			temp = someBools[n];
+			someBools[n] = someBools[d];
+			someBools[d] = temp;
+            n--;
+            d++; }
 		/*
 		 * ASSIGNMENT:
 		 * Replace the operations above with a while loop
 		 */
 
 		if (Test.testBools(someBools)) {
-			System.out.print("Your booleans are in proper order!\n");
-		} else {
-			System.out.print("Something in the while loop…\n");
-			System.exit(0);
-		}
+			System.out.print("Your booleans are in proper order!\n"); } 
+			else {
+			 System.out.print ("Something in the while loop…\n");
+			 System.exit(0); }
 
-		int[] numArray = new int[someBools.length];
-		// This is known as an in-line conditional
-		// learn more here: http://www.cafeaulait.org/course/week2/43.html
+		
+		
+		/*This is known as an in-line conditional
+		 learn more here: http://www.cafeaulait.org/course/week2/43.html
+        */
+		 
+		 //  Starts here
+		    
 
-		// Starts here
+		
+        int[] numArray = new int[someBools.length];
+		
+		/*
+		 if (!someBools[0]) {
+			  numArray[0] = 1; } 
+		   else {
+			  numArray[0] = 0; }
+		*/
+		
+		/*
 		numArray[0] = !someBools[0] ? 1 : 0;
 		numArray[1] = !someBools[1] ? 1 : 0;
 		numArray[2] = !someBools[2] ? 1 : 0;
@@ -60,18 +84,22 @@ public class Loops extends Object {
 		numArray[5] = !someBools[5] ? 1 : 0;
 		numArray[6] = !someBools[6] ? 1 : 0;
 		numArray[7] = !someBools[7] ? 1 : 0;
-		// Ends here
-
-
+		*/
+		
+		//Ends here
+	
+		int x = 0; 
+		int y = 0;
+		for (x <= 7; x++) {
+			numArray[x] = !someBools[y] ? 1: 0; }
 		/*
 		 * ASSIGNMENT:
 		 * Replace the operations above with a for loop
 		 */
 
 		if (Test.testInts(numArray)) {
-			System.out.print("And you nailed the number array!\n");
-		} else {
-			System.out.print("Issue with the numbers…\n");
-		}
+			System.out.print("And you nailed the number array!\n"); } 
+			else {
+			System.out.print("Issue with the numbers…\n"); }
 	}
 }
