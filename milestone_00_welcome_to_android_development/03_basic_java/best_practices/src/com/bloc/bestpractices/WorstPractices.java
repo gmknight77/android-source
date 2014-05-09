bcae84d9034515d59ed8a6b625cb0a0e603656cd
ERH <gmknight77@gmail.com>
2014-05-09 11:37:50 -0400

    Update3 to Worst Practices ERH

--- milestone_00_welcome_to_android_development/03_basic_java/best_practices/src/com/bloc/bestpractices/WorstPractices.java
+++ milestone_00_welcome_to_android_development/03_basic_java/best_practices/src/com/bloc/bestpractices/WorstPractices.java
@@ -4,36 +4,33 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class WorstPractices extends Object {
+public class WorstPractices extends Object{
 
 // EDIT BELOW
 
 	public static void main(String [] args) {
-		int magicNumber worstPractices.animals(false);
-		magicNumber*= 5;
-			if (magicNumber > 18) {
-									while(magicNumber > 0)
-								{
-				magicNumber--;
-									}
-			}
+		int magicNumber = worstPractices.animals(false);
+		magicNumber *= 5;
+		 if (magicNumber > 18) {
+			while(magicNumber > 0) {
+				magicNumber--;}
+		 }
 	}
 
-// animals
-// this method takes in a single parameter, yeswellheresthething. 
-//Using a very elaborate and complex algorithm, it calculate a magic number
-// yeswellheresthething: a seed which helps generate the magic number
-// returns: a magical number
-private static int animals(boolean yeswellheresthething)
-{
+/*  animals
+	this method takes in a single parameter, yeswellheresthething. 
+	Using a very elaborate and complex algorithm, it calculate a magic number
+ 	yeswellheresthething: a seed which helps generate the magic number
+ 	returns: a magical number
+*/
+private static int animals(boolean theThing) {
 		/*
 			Start off with one of these
 		*/
-	int aInt = theThing ? 34 : 21;
+	int anInt = theThing ? 34 : 21;
 	float sparklesFairy = .5f;
-	for (int brown = 0; brown < aInt; brown++) 
-		{ sparklesFairy *= aInt;
-			} 
+	for (int brown = 0; brown < anInt; brown++) { 
+		 sparklesFairy *= anInt; } 
 	return (int) sparklesFairy * aInt;
 }
 
